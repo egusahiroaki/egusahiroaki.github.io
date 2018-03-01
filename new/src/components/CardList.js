091ae1c482631c972a4a1fa206bb9e8c998b2a79
egusahiroaki <egusa.hiroaki@gmail.com>
2018-03-02 02:06:11 +0900

    create content file

--- new/src/components/CardList.js
+++ new/src/components/CardList.js
@@ -2,86 +2,10 @@ import React, { Component } from 'react';
 
 import NewCard from './NewCard';
 
-import dotInt from '../images/dot_int.png';
-import egg from '../images/egg.png';
-import face from '../images/face.png';
-import mtg from '../images/markdown_table_generator.png';
-import brush from '../images/brush.png';
-import quick from '../images/quick_sort.png';
-import bubble from '../images/bubble_sort.png';
-
 import Modal from 'react-modal';
 import DetailModal from './DetailModal';
 
-
-var testItems = [
-  {
-    image: dotInt,
-    title: 'Dot Integer',
-    description:'', // Various column sizes and integer values ​​of row sizes are displayed.
-    url: 'https://egusahiroaki.github.io/dot_int/',
-    tags: [
-      {title: 'Canvas', color:'#ff8686'}
-    ]
-  },
-  {
-    image: egg,
-    title:'Egg',
-    description:'',
-    url: 'https://egusahiroaki.github.io/friedEgg1/',
-    tags: [
-      {title: 'Canvas', color:'#ff8686'}
-    ]
-  },
-  {
-    image: face,
-    title:'Face',
-    description:'',
-    url: 'https://egusahiroaki.github.io/Face/',
-    tags: [
-      {title: 'Canvas', color:'#ff8686'}
-    ]
-  },
-  {
-    image: mtg,
-    title:'markdown table',
-    description:'',
-    url: 'https://egusahiroaki.github.io/markdownTableGenerator/#/',
-    tags: [
-      {title: 'Vue.js', color:'#ff8686'}
-    ]
-  },
-  {
-    image: brush,
-    title:'brush',
-    description:'You can write Anything.',
-    url: 'https://egusahiroaki.github.io/brush01/',
-    tags: [
-      {title: 'Canvas', color:'#ff8686'}
-    ]
-  },
-  {
-    image: quick,
-    title: 'Quick Sort',
-    description:'Visualize sorting algorithm called quick sort',
-    url: 'https://egusahiroaki.github.io/quickSort/',
-    tags: [
-      {title: 'Canvas', color:'#ff8686'},
-      {title: 'Algorithm', color:'#f7c770'}
-    ]
-  },
-  {
-    image: bubble,
-    title: 'Bubble Sort',
-    description:'Visualize sorting algorithm called bubble sort.',
-    url: 'https://egusahiroaki.github.io/bubbleSort/',
-    tags: [
-      {title: 'Canvas', color:'#ff8686'},
-      {title: 'Algorithm', color:'#f7c770'}
-    ]
-  }
-
-]
+import Items from '../content';
 
 const customStyles = {
   content : {
@@ -143,7 +67,7 @@ class CardList extends Component {
           />
         </Modal>
 
-        {testItems.map((item, index) => {
+        {Items.map((item, index) => {
           return <NewCard
             click={() => this.openModal(item)}
             key={item.title}
---
+++ new/src/content.js
@@ -0,0 +1,77 @@
+import dotInt from './images/dot_int.png';
+import egg from './images/egg.png';
+import face from './images/face.png';
+import mtg from './images/markdown_table_generator.png';
+import brush from './images/brush.png';
+import quick from './images/quick_sort.png';
+import bubble from './images/bubble_sort.png';
+
+const Items = [
+  {
+    image: dotInt,
+    title: 'Dot Integer',
+    description:'', // Various column sizes and integer values ​​of row sizes are displayed.
+    url: 'https://egusahiroaki.github.io/dot_int/',
+    tags: [
+      {title: 'Canvas', color:'#ff8686'}
+    ]
+  },
+  {
+    image: egg,
+    title:'Egg',
+    description:'',
+    url: 'https://egusahiroaki.github.io/friedEgg1/',
+    tags: [
+      {title: 'Canvas', color:'#ff8686'}
+    ]
+  },
+  {
+    image: face,
+    title:'Face',
+    description:'',
+    url: 'https://egusahiroaki.github.io/Face/',
+    tags: [
+      {title: 'Canvas', color:'#ff8686'}
+    ]
+  },
+  {
+    image: mtg,
+    title:'markdown table',
+    description:'',
+    url: 'https://egusahiroaki.github.io/markdownTableGenerator/#/',
+    tags: [
+      {title: 'Vue.js', color:'#ff8686'}
+    ]
+  },
+  {
+    image: brush,
+    title:'brush',
+    description:'You can write Anything.',
+    url: 'https://egusahiroaki.github.io/brush01/',
+    tags: [
+      {title: 'Canvas', color:'#ff8686'}
+    ]
+  },
+  {
+    image: quick,
+    title: 'Quick Sort',
+    description:'Visualize sorting algorithm called quick sort',
+    url: 'https://egusahiroaki.github.io/quickSort/',
+    tags: [
+      {title: 'Canvas', color:'#ff8686'},
+      {title: 'Algorithm', color:'#f7c770'}
+    ]
+  },
+  {
+    image: bubble,
+    title: 'Bubble Sort',
+    description:'Visualize sorting algorithm called bubble sort.',
+    url: 'https://egusahiroaki.github.io/bubbleSort/',
+    tags: [
+      {title: 'Canvas', color:'#ff8686'},
+      {title: 'Algorithm', color:'#f7c770'}
+    ]
+  }
+]
+
+export default Items;
