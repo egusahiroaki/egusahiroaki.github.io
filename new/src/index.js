39f7069181dea9691088eda8b34d6447dcc432a6
egusahiroaki <egusa.hiroaki@gmail.com>
2018-03-02 01:30:53 +0900

    delete serivice worker

--- new/src/index.js
+++ new/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
 // "Gotham Rounded A", "Gotham Rounded B", "Gotham Rounded", "Helvetica Neue", Helvetica, Arial, sans-serif
